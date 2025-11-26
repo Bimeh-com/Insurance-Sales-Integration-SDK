cbbeaf76e0abf35cd8ababd5d7797556538d3ece
Ashkan Afsharpour <ashkan77af@gmail.com>
2025-11-26 12:21:11 +0330

    added Main Client Builder and Options

---
+++ Bimehcom.Client/BimehcomClientBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Bimehcom.Core;
+using Bimehcom.Core.Options;
+using System;
+
+namespace Bimehcom.Client
+{
+    public class BimehcomClientBuilder
+    {
+        private BimehcomClientOptions _options;
+
+        public BimehcomClientBuilder(Action<BimehcomClientOptions> options)
+        {
+            _options = new BimehcomClientOptions();
+            options(_options);
+            _options.Validate();
+        }
+
+        public IBimehcomClient Build()
+        {
+            return new BimehcomClient();
+        }
+    }
+}
--- Bimehcom.Samples/Program.cs
+++ Bimehcom.Samples/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Bimehcom.Client;
+using Bimehcom.Core;
+
+IBimehcomClient client = new BimehcomClientBuilder((opt)  =>
+{
+    opt.ApiKey = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx";
+    opt.BaseApiUrl = new Uri("https://coreapi.bimeh.com");
+    opt.ApiVersion = "v1";
+}).Build();
