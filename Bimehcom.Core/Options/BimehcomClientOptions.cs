5f1f6cb679852b58d3c53f1541f9ac3961075a94
Ashkan Afsharpour <43031669+AshkanAfsharpour@users.noreply.github.com>
2025-11-26 12:25:32 +0330

    added Main Client Builder and Options (#11)

---
+++ Bimehcom.Core/Options/BimehcomClientOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bimehcom.Core.Options
+{
+    public class BimehcomClientOptions
+    {
+        public string ApiKey { get; set; }
+        public Uri BaseApiUrl{ get; set; }
+        public string ApiVersion { get; set; }
+
+        public void Validate()
+        {
+            // Todo
+            // Add validations and throw the corresponding exceptions after Core Exception logic is implemented
+        }
+    }
+}
