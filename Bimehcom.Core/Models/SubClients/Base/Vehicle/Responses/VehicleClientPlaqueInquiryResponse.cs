62b5deb9f12e0e8b8e3ed5ee2292d15def44368c
Ashkan Afsharpour <43031669+AshkanAfsharpour@users.noreply.github.com>
2025-12-01 13:59:39 +0330

    implemented Vehicle Plaque Inquiry (#29)

---
+++ Bimehcom.Core/Models/SubClients/Base/Vehicle/Responses/VehicleClientPlaqueInquiryResponse.cs
@@ -0,0 +1,124 @@
+﻿using Bimehcom.Core.Models.Abstraction;
+using Bimehcom.Core.Models.Base.Inquiry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bimehcom.Core.Models.SubClients.Base.Vehicle.Responses
+{
+    public class VehicleClientPlaqueInquiryResponse : IBimehcomApiResponse
+    {
+        /// <summary>
+        /// شناسه وضعیت بیمه نامه قبلی
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public PreviousInsuranceStatus? PreviousInsuranceStatusId { get; set; }
+
+        /// <summary>
+        /// شرکت بیمه گر قبلی
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public int? PreviousCompanyId { get; set; }
+
+        /// <summary>
+        /// شناسه سال ساخت (میلادی)
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public int? ProductionYearId { get; set; }
+
+        /// <summary>
+        /// تاریخ اتمام بیمه نامه قبلی
+        /// </summary>
+        public DateTime? PreviousExpirationDate { get; set; }
+
+        /// <summary>
+        /// تاریخ شماره گذاری / ترخیص خودروی صفر، فاقد بیمه نامه یا وارداتی
+        /// </summary>
+        public DateTime? ReleaseDate { get; set; }
+        /// <summary>
+        /// شناسه برند خودرو
+        /// </summary>
+        public int? BrandId { get; set; }
+
+        /// <summary>
+        /// مدل خودرو
+        /// </summary>
+        public string ModelTitle { get; set; }
+
+        /// <summary>
+        /// نوع خودرو
+        /// </summary>
+        public int? VehicleCategoriyId { get; set; }
+
+        /// <summary>
+        /// شناسه مدل خودرو
+        /// </summary>
+        public int? ModelId { get; set; }
+
+        /// <summary>
+        /// شناسه درصد تخفیف شخص ثالث بیمه نامه قبلی 
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public int? ThirdPartyDiscountId { get; set; }
+
+        /// <summary>
+        /// شناسه درصد تخفیف حوادث راننده بیمه نامه قبلی
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public int? DriverDiscountId { get; set; }
+
+        /// <summary>
+        /// شناسه مدت زمان بیمه نامه درخواستی
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public short? DurationId { get; set; }
+
+        /// <summary>
+        /// شناسه تعداد خسارت جانی بیمه نامه قبلی
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public short? LifeLossId { get; set; }
+
+        /// <summary>
+        /// شناسه تعداد خسارت مالی بیمه نامه قبلی
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public short? PropertyLossId { get; set; }
+
+        /// <summary>
+        /// شناسه تعداد خسارت حوادث راننده
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public short? DriverLossId { get; set; }
+
+        /// <summary>
+        /// شناسه نوع پوشش مالی بیمه نامه درخواستی
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public int? CoverageTypeId { get; set; }
+
+        /// <summary>
+        /// شناسه مدت زمان بیمه نامه قبلی
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public int? PreviousDurationId { get; set; }
+
+        /// <summary>
+        /// نوع استفاده
+        /// دریافت شده از سرویس basic-data
+        /// </summary>
+        public int? UsingTypeId { get; set; }
+
+        /// <summary>
+        /// پلاک رمزنگاری شده
+        /// </summary>
+        public string EncryptedPlaque { get; set; }
+        public bool HasEndorsement { get; internal set; }
+
+        public bool Imported { get; set; }
+
+        public int? ProductionYear { get; set; }
+        public byte ReferenceId { get; set; }
+
+    }
+}
